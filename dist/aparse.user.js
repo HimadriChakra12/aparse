52eb437aace8916e0ec9f4aa9f0505726edd9089
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-08-28 02:01:15 +0600

    Version 7.14.1 - a little remux revamp

--- dist/aparse.user.js
+++ dist/aparse.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Apars Classroom HLS Saver
 // @namespace    apars-aparse
-// @version      7.14.0
+// @version      7.14.1
 // @description  Download 720p HLS stream (forced level select + ffmpeg.wasm remux)
 // @match        https://*.aparsclassroom.com/*
 // @match        https://iframe.mediadelivery.net/*
